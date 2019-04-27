0dfa6ef086c9090594bcf368710ba8a7f3a38758
Swanson, Steven <srswan1@ilstu.edu>
2019-04-27 13:45:30 -0500

    Added alarm functionality to go off at noon

--- app/src/main/java/com/example/alarmclock/AddAlarmActivity.java
+++ app/src/main/java/com/example/alarmclock/AddAlarmActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import java.util.Calendar;
 
 public class AddAlarmActivity extends AppCompatActivity {
 
@@ -17,6 +18,10 @@ public class AddAlarmActivity extends AppCompatActivity {
         addNewAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Set the alarm to start at approximately 12:00 p.m.
+                Calendar calendar = Calendar.getInstance();
+                calendar.setTimeInMillis(System.currentTimeMillis());
+                calendar.set(Calendar.HOUR_OF_DAY, 12);
 
             }
         });
