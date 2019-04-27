e41deb043b3121a5cd961b9f2def8083ba48779d
Slama, Aaron <amslama@ilstu.edu>
2019-04-26 20:06:13 -0500

    Added base functionality for add alarms

--- app/src/main/java/com/example/alarmclock/MainScreen.java
+++ app/src/main/java/com/example/alarmclock/MainScreen.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -13,8 +14,18 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        Toolbar toolbar = findViewById(R.id.alarmLabel);
+
+        Button addAlarm = (Button) findViewById(R.id.addAlarm);
+        addAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.alarmLabel);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
